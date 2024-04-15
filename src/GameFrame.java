7baf2d2e02fd0712cc7b6578d600f8b7cc9e3a08
_ <@gmail.com>
2024-04-15 13:55:43 -0400

    Implemented KeyListener and managed player speed/movement on screen.

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.JFrame;
+import java.awt.*;
+
 public class GameFrame extends JFrame{
     private JFrame window;
     private GamePanel gamePanel;
@@ -9,11 +11,11 @@ public class GameFrame extends JFrame{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("FFA");
-        gamePanel = new GamePanel();
+        GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
         window.addKeyListener(gamePanel);
         window.pack();
-        window.setSize(1920, 1024);
+        window.setSize(1920, 1024); // 1920 1024
         window.setLocationRelativeTo(null);
         window.setVisible(true);
         gamePanel.startGameThread();
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,12 +7,19 @@ import java.awt.event.MouseListener;
 
 public class GamePanel extends JPanel implements Runnable, MouseListener, KeyListener {
     Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+    double FPS = 60;
+
+    int playerX = 100, playerY = 100, speed = 10;
+
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(1920, 1024));
-        this.setBackground(Color.black);
+        this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
         this.addMouseListener(this);
-        this.addKeyListener(this);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -21,16 +28,45 @@ public class GamePanel extends JPanel implements Runnable, MouseListener, KeyLis
     }
     @Override
     public void run(){
+        double drawInterval = 1000000000/FPS; // # of times that you need to draw within a second, to maintain 60fps
+        double drawNext = System.nanoTime() + drawInterval;
         while (true){
             update();
-//            repaint();
+            repaint();
+            try{
+                double remainingTime = drawNext - System.nanoTime(); // how much time is left before the next draw
+                remainingTime = remainingTime / 1000000;
+                if (remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+                drawNext += drawInterval;
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
     public void update(){
-        System.out.println("test");
+        if (keyHandler.up){
+            playerY -= speed;
+        }
+        else if (keyHandler.left){
+            playerX -= speed;
+        }
+        else if (keyHandler.down){
+            playerY += speed;
+        }
+        else if (keyHandler.right){
+            playerX += speed;
+        }
     }
     public void paintComponent(Graphics g){
-        super.paint(g);
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(playerX, playerY, 64, 64);
+        g2.dispose();
     }
     @Override
     public void keyTyped(KeyEvent e) {}
